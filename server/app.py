1c18d39a2c4385bc9dfcb52ea1148547865494bd
CynaraCosta <cvocac@cin.ufpe.br>
2025-01-16 18:28:54 -0300

    add routes

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return '<h1>Hello world!</h1>'
+
 
 @app.route('/hello')
 def hello():
@@ -12,25 +14,71 @@ def hello():
     response.status_code = 202
     response.headers['content-type'] = 'text/plain'
     return response
-    
 
-@app.route('/greet/<name>')
-def greet(name):
-    return f'Hello {name}'
 
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    return f'{number1} + {number2} = {number1 + number2}'
+@app.route('/v1/home', methods=['GET'])
+def get_widgets():
+    response_data = {
+        "widgets": [
+            {
+                "id": "explorerCarousel",
+                "state": "loading",
+                "path": "/v1/explorer-carousel",
+                "data": {},
+                "style": {
+                    "bounds": {
+                        "top": "soma_spacing_inline_md",
+                        "left": "soma_spacing_inline_xs",
+                        "right": "soma_spacing_inline_xs"
+                    }
+                }
+            },
+            {
+                "id": "historyCards",
+                "state": "loading",
+                "path": "/v1/history-cards",
+                "data": {},
+                "style": {
+                    "bounds": {
+                        "top": "soma_spacing_inline_md",
+                        "left": "soma_spacing_inline_xs",
+                        "right": "soma_spacing_inline_xs"
+                    }
+                }
+            }
+        ]
+    }
 
-@app.route('/handle_url_params')
-def handle_params():
-    if 'greeting' in request.args.keys and 'name' in request.args.keys:
-        greeting = request.args.get('greeting')
-        name = request.args.get('name')
-        return f'{greeting} {name}'
-    else:
-        return 'Some parameters are missing'
-    
+    return jsonify(response_data)
+
+
+@app.route('/v1/explorer-carousel', methods=['GET'])
+def get_explorer_carousel():
+    response_data = {
+        'title': 'Explorar',
+        'cards': [
+            {
+                "title": "Chat AI",
+                "subtitle": "Start New Conversation",
+                "icon": "message",
+                "deeplink": "/chatia"
+            }
+        ]
+    }
+
+    return jsonify(response_data)
+
+
+@app.route('/v1/history-cards', methods=['GET'])
+def get_history_cards():
+    response_data = {
+        "title": "Histórico",
+        "subtitle": "Ver mais",
+        "cards": []
+    }
+
+    return jsonify(response_data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
